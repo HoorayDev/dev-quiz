f83d266df8fe735a69a0da656e08dbd5810c595b
2Ruk <94098919+2Ruk@users.noreply.github.com>
2023-05-08 01:50:15 +0900

    feat: add common module
    add typeorm, cacheModule, ttl Module
    ref: #8

--- server/libs/share-library/src/share-library.module.ts
+++ server/libs/share-library/src/share-library.module.ts
@@ -1,8 +1,29 @@
-import { Module } from '@nestjs/common';
-import { ShareLibraryService } from './share-library.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { HttpException, Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  providers: [ShareLibraryService],
-  exports: [ShareLibraryService],
+  imports:[
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    CacheModule.register({
+      ttl: 3000,
+      max: 10,
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRoot({ ttl: 1, limit: 60 }),
+  ],
+  providers: [],
+  exports: [],
 })
 export class ShareLibraryModule {}
