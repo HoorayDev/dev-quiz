428168333be14cdc09295b14e13a7cb1b0cf5d62
2Ruk <leerukdev@gmail.com>
2023-05-15 21:34:11 +0900

    feat[#24]: add /health

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('app')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    return this.appService.getHello();
+  }
+
+  // Endpoint: Health Check
+  @Get('/health')
+  healthCheck() {
     return this.appService.getHello();
   }
 }
