6c60fb4e0448b3d58ad2b992297120b4406f715d
2Ruk <leerukdev@gmail.com>
2023-05-15 22:46:50 +0900

    remove[#24]: default dto

--- server/src/quiz-set/dto/update-quiz-set.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateQuizSetDto } from './create-quiz-set.dto';
-
-export class UpdateQuizSetDto extends PartialType(CreateQuizSetDto) {}
