2611190cc57afc042d2735686984aba34f56c713
2Ruk <leerukdev@gmail.com>
2023-05-15 22:04:29 +0900

    refactor[#23]: add trans interceptor

---
+++ server/libs/share-library/interceptor/Transform.interceptor.ts
@@ -0,0 +1,22 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<T, any> {
+  intercept(context: ExecutionContext, next: CallHandler<T>): Observable<any> {
+    const ctx = context.switchToHttp();
+    const response = ctx.getResponse();
+    return next.handle().pipe(
+      map((data) => ({
+        statusCode: response.statusCode,
+        data: data,
+      })),
+    );
+  }
+}
