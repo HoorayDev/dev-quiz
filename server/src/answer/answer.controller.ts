175d4afb8e02492b59b25d44058beb54df3bd367
2Ruk <leerukdev@gmail.com>
2023-05-15 23:06:55 +0900

    form: TODO 정리 ( answer )

--- server/src/answer/answer.controller.ts
+++ server/src/answer/answer.controller.ts
@@ -19,28 +19,20 @@ import {
 export class AnswerController {
   constructor(private readonly answerService: AnswerService) {}
 
+  // TODO: 정답 제출 ( quiz-id, value 에 대한 Array 전달 받음 )
+  // TODO: 정답 여부 체크 in Answer 데이터
+  // TODO: quiz-response 에 O / X 기록
+  // TODO: 선택지 2 ( 그냥 성공 여부를 보낼지, get :id )와 같게 동작할지, 전자로 할 가능 성 높음
   @Post()
   create(@Body() createAnswerDto: CreateAnswerDto) {
     return this.answerService.create(createAnswerDto);
   }
 
-  @Get()
-  findAll() {
-    return this.answerService.findAll();
-  }
+  // TODO: 로그연 여부 체크
 
+  // TODO: 정답 여부에 대한 정보를 보내주어야 함
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.answerService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAnswerDto: UpdateAnswerDto) {
-    return this.answerService.update(+id, updateAnswerDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.answerService.remove(+id);
   }
 }
