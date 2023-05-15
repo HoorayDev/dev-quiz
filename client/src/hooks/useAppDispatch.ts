01cac4519c140fb553665c31e05c3002adfe191a
Jay <yoojamer@gmail.com>
2023-05-15 23:06:19 +0900

    chore[#4]: setting up tsconfig

--- client/src/hooks/useAppDispatch.ts
+++ client/src/hooks/useAppDispatch.ts
@@ -1,5 +1,5 @@
 import { useDispatch } from 'react-redux';
 import type { TypedUseSelectorHook } from 'react-redux';
-import type { RootState, AppDispatch } from '../store/index';
+import type { RootState, AppDispatch } from '~/store/index';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
--- client/src/store/reducer.ts
+++ client/src/store/reducer.ts
@@ -1,12 +1,13 @@
 import {combineReducers} from 'redux';
 import { HYDRATE } from 'next-redux-wrapper';
-import counterSlice from './countSlice';
+import counterSlice from '~/store/countSlice';
+import { Reducer } from 'react';
 
-const combinedReducer = combineReducers({
+const combinedReducer: any = combineReducers({
     counter: counterSlice,
 });
 
-const rootReducer: typeof combinedReducer = (state, action) => {
+const rootReducer: typeof combinedReducer = (state: any, action: any) => {
     if (action.type === HYDRATE) {
         const nextState = {
             ...state,
