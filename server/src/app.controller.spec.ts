80c4ce4b79ba92ca91a64ac7b8d77e745fdb0837
2Ruk <leerukdev@gmail.com>
2023-05-16 22:35:30 +0900

    test[#24]: first test code at HealthCheck
    저의 개발자로서의 첫번째, test code 입니다.

--- server/src/app.controller.spec.ts
+++ server/src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthCheckDto } from '@app/share-library/dto/health.dto';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -14,9 +15,25 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
+  describe('getHello', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+
+  describe('healthCheck', () => {
+    it('should return "{ result: true }"', () => {
+      const healthCheck = appController.healthCheck;
+
+      // return value
+      const expected: HealthCheckDto = {
+        result: true,
+      };
+
+      // check return type
+      expect(healthCheck()).toEqual(expected);
+      // check return property
+      expect(healthCheck()).toHaveProperty('result');
     });
   });
 });
