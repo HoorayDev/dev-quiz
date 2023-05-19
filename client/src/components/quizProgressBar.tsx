981179cac5505750953482d79395004b13e639a4
Jay <yoojamer@gmail.com>
2023-05-20 04:44:48 +0900

    feat[#29]: add quiz progress bar

---
+++ client/src/components/quizProgressBar.tsx
@@ -0,0 +1,21 @@
+import styles from '~/components/quizProgressBar.module.scss'
+
+interface QuizProgressBarProps {
+    maxValue: number;
+    currentValue: number;
+}
+
+const QuizProgressBar = (props: QuizProgressBarProps) => {
+    const { maxValue, currentValue } = props
+
+    return (
+        <div className={styles.progressBarWrapper}>
+            <div className={`${styles.currentProgress} ${styles[`q${currentValue}`]}`} />
+            <div className={styles.tooltipWrapper}>
+                <span className={`${styles.tooltip} ${styles[`q${currentValue}`]}`}>{currentValue}/{maxValue}</span>
+            </div>
+        </div>
+    )
+}
+
+export default QuizProgressBar
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,25 +1,45 @@
-import wrapper from '../store/index';
-import { useAppSelector } from '../hooks/useAppSelector';
-import { useAppDispatch } from '../hooks/useAppDispatch';
-import { decrement, increment } from '../store/countSlice';
-import { RootState } from '../store'
-import styles from '../styles/index.module.scss'
+import { useMemo } from "react";
+import wrapper from '~/store/index';
+import { useAppSelector } from '~/hooks/useAppSelector';
+import { useAppDispatch } from '~/hooks/useAppDispatch';
+import { decrement, increment } from '~/store/countSlice';
+import { RootState } from '~/store'
+import styles from '~/pages/index.module.scss'
+import QuizProgressBar from '~/components/quizProgressBar'
+
+const categoryList = ['Javascript', 'Comming Soon..', 'Comming Soon..', 'Comming Soon..']
 
 const Index =()=>{
-  const dispatch = useAppDispatch();
-  const { value } = useAppSelector((state:RootState) => state.counter);
+    const dispatch = useAppDispatch();
+    const { value } = useAppSelector((state:RootState) => state.counter);
 
-  return (
-      <div>
+    const categoryListCard = useMemo(() => {
+        return categoryList.map((categoryName) => {
+            const isCommingSoon = categoryName === 'Comming Soon..'
+            const cardStyle = isCommingSoon ?
+                `${styles.card} ${styles.commingSoon}` :
+                styles.card
+            const cardUrl = `${categoryName}`
+
+            return (
+                <button className={cardStyle} disabled={isCommingSoon} onClick={() => console.log(cardUrl)}>
+                    {categoryName}
+                </button>
+            )
+        })
+    }, [categoryList])
+
+    return (
         <div>
-          <span className={styles.movieList}>{value}</span>
-          <div>
-            <button onClick={()=>dispatch(increment())}>+</button>
-            <button onClick={()=>dispatch(decrement())}>-</button>
-          </div>
+            <QuizProgressBar maxValue={10} currentValue={5}/>
+            <div className={styles.categoryWrapper}>{categoryListCard}</div>
+            {/*<span>{value}</span>*/}
+            {/*<div>*/}
+            {/*    <button onClick={()=>dispatch(increment())}>+</button>*/}
+            {/*    <button onClick={()=>dispatch(decrement())}>-</button>*/}
+            {/*</div>*/}
         </div>
-      </div>
-  )
+    )
 }
 
 export default Index;
