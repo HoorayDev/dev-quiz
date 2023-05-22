709b03630c3152c566444a9b718d4efa11337427
Jay <85284246+Yu-jae-min@users.noreply.github.com>
2023-05-22 17:29:40 +0900

    feat[#29]: add modal and input component (#41)

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -5,12 +5,16 @@ import { useAppDispatch } from '~/hooks/useAppDispatch';
 import { decrement, increment } from '~/store/countSlice';
 import { RootState } from '~/store'
 import styles from '~/pages/index.module.scss'
+import { useState } from 'react'
+import ModalPortal from '~/components/Modal/modalPortal';
+import LoginModal from '~/components/Modal/LoginModal/LoginModal';
 
 const categoryList = ['Javascript', 'Comming Soon..', 'Comming Soon..', 'Comming Soon..']
 
 const Index =()=>{
   const dispatch = useAppDispatch();
   const { value } = useAppSelector((state:RootState) => state.counter);
+  const [modalOpen, setModalOpen] = useState<boolean>(); // TODO : modal 사용 예시를 위해 넣어둠
 
   const categoryListCard = useMemo(() => {
       return categoryList.map((categoryName) => {
@@ -30,7 +34,13 @@ const Index =()=>{
 
   return (
       <div>
+          <button onClick={() => setModalOpen(true)}>로그인</button>
           <div className={styles.categoryWrapper}>{categoryListCard}</div>
+          {modalOpen && (
+              <ModalPortal>
+                  <LoginModal onClose={() => setModalOpen(false)} />
+              </ModalPortal>
+          )}
           {/*<span>{value}</span>*/}
           {/*<div>*/}
           {/*    <button onClick={()=>dispatch(increment())}>+</button>*/}
