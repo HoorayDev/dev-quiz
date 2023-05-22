a0f6731274c716640289f8accd8a55dddc8451af
JungSWon <sw.onandon@gmail.com>
2023-05-22 16:01:06 +0900

    feat[#30] polish app header

--- client/src/components/floating/appHeader.tsx
+++ client/src/components/floating/appHeader.tsx
@@ -1,15 +1,12 @@
 import { FC } from "react";
 import styles from '~/components/floating/appHeader.module.scss';
+import Logo from '~/images/hooray_logo.svg'
 
 interface AppHeaderProps {}
 
 const AppHeader: FC<AppHeaderProps> = () => {
   return <nav className={`${styles.nav} ${styles.flexRow}`}>
-    <div className={`${styles.flexRow} ${styles.navTitle}`}>
-      {/* TODO : svg */}
-      <div>🥑</div>
-      <div>Hooray</div>
-    </div>
+    <Logo />
     <div className={`${styles.flexRow} ${styles.navMenu}`}>
       <p>홈</p>
       <p>팀소개</p>
