b38903d6f34739149de2bbd0e02195a712a85ae5
Jay <85284246+Yu-jae-min@users.noreply.github.com>
2023-05-22 17:39:16 +0900

    feat[#29]: add quiz progress bar (#42)

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -5,6 +5,7 @@ import { useAppDispatch } from '~/hooks/useAppDispatch';
 import { decrement, increment } from '~/store/countSlice';
 import { RootState } from '~/store'
 import styles from '~/pages/index.module.scss'
+import QuizProgressBar from '~/components/quizProgressBar'
 import { useState } from 'react'
 import ModalPortal from '~/components/Modal/modalPortal';
 import LoginModal from '~/components/Modal/LoginModal/LoginModal';
@@ -16,38 +17,39 @@ const Index =()=>{
   const { value } = useAppSelector((state:RootState) => state.counter);
   const [modalOpen, setModalOpen] = useState<boolean>(); // TODO : modal 사용 예시를 위해 넣어둠
 
-  const categoryListCard = useMemo(() => {
-      return categoryList.map((categoryName) => {
-          const isCommingSoon = categoryName === 'Comming Soon..'
-          const cardStyle = isCommingSoon ?
-              `${styles.card} ${styles.commingSoon}` :
-              styles.card
-          const cardUrl = `${categoryName}`
+    const categoryListCard = useMemo(() => {
+        return categoryList.map((categoryName) => {
+            const isCommingSoon = categoryName === 'Comming Soon..'
+            const cardStyle = isCommingSoon ?
+                `${styles.card} ${styles.commingSoon}` :
+                styles.card
+            const cardUrl = `${categoryName}`
 
-          return (
-              <button className={cardStyle} disabled={isCommingSoon} onClick={() => console.log(cardUrl)}>
-                  {categoryName}
-              </button>
-          )
-      })
-  }, [categoryList])
+            return (
+                <button className={cardStyle} disabled={isCommingSoon} onClick={() => console.log(cardUrl)}>
+                    {categoryName}
+                </button>
+            )
+        })
+    }, [categoryList])
 
-  return (
-      <div>
-          <button onClick={() => setModalOpen(true)}>로그인</button>
-          <div className={styles.categoryWrapper}>{categoryListCard}</div>
-          {modalOpen && (
-              <ModalPortal>
-                  <LoginModal onClose={() => setModalOpen(false)} />
-              </ModalPortal>
-          )}
-          {/*<span>{value}</span>*/}
-          {/*<div>*/}
-          {/*    <button onClick={()=>dispatch(increment())}>+</button>*/}
-          {/*    <button onClick={()=>dispatch(decrement())}>-</button>*/}
-          {/*</div>*/}
-      </div>
-  )
+    return (
+        <div>
+            <QuizProgressBar maxValue={10} currentValue={5}/>
+            <div className={styles.categoryWrapper}>{categoryListCard}</div>
+            <button onClick={() => setModalOpen(true)}>로그인</button>
+            {modalOpen && (
+                <ModalPortal>
+                    <LoginModal onClose={() => setModalOpen(false)} />
+                </ModalPortal>
+            )}
+            {/*<span>{value}</span>*/}
+            {/*<div>*/}
+            {/*    <button onClick={()=>dispatch(increment())}>+</button>*/}
+            {/*    <button onClick={()=>dispatch(decrement())}>-</button>*/}
+            {/*</div>*/}
+        </div>
+    )
 }
 
 export default Index;
