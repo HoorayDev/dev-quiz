6a8352a6279dea22be2e3afd6443e61bc6d9e878
JungSWon <sw.onandon@gmail.com>
2023-05-28 18:40:40 +0900

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,24 @@
 const path = require('path')
 
 const nextConfig = {
+    image:{
+        // limit of 25 deviceSizes values
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        // limit of 25 imageSizes values
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        // limit of 50 domains values
+        domains: ['http://localhost:3000'],
+        // path prefix for Image Optimization API, useful with `loader`
+        path: '/_next/image',
+        // loader can be 'default', 'imgix', 'cloudinary', 'akamai', or 'custom'
+        loader: 'default',
+        // disable static imports for image files
+        disableStaticImages: false,
+        // minimumCacheTTL is in seconds, must be integer 0 or more
+        minimumCacheTTL: 60,
+        // ordered list of acceptable optimized image formats (mime types)
+        formats: ['image/webp'],
+    },
     experimental: {
         appDir: true
     },
