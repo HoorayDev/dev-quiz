c376a08235a1839202aeb0f8158e47337fb8104d
2Ruk <leerukdev@gmail.com>
2023-05-29 15:42:07 +0900

    test[#24]: add createUser TestCode

--- server/src/user/user.controller.spec.ts
+++ server/src/user/user.controller.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { CreateUserDto } from '@api/user/dto/user.input.dto';
+import { faker } from '@faker-js/faker';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +15,21 @@ describe('UserController', () => {
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create User', () => {
+    it('should return a new user', () => {
+      const creatUser: CreateUserDto = {
+        name: faker.person.fullName(),
+      };
+      expect(controller.create(creatUser)).toBe(
+        `This action adds a new user ${creatUser.name}`,
+      );
+    });
   });
 });
