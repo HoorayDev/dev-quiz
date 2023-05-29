e930554b18cf38157e75b9ab7c8353bb1db9aac4
JungSWon <sw.onandon@gmail.com>
2023-05-29 16:27:38 +0900

    fix[#30] create splash && test on _app

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import { AppProps } from "next/app";
+import { useRouter } from 'next/router';
 import { Provider } from 'react-redux';
 import wrapper from '~/store/index';
 import Layout from '~/components/HOC/Layout';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { Splash } from '~/pages/splash';
 import '~/styles/index.scss'
 
 interface DefaultStaticProps {
@@ -12,7 +14,17 @@ interface DefaultStaticProps {
 const _App: FC<AppProps> = ({ Component, pageProps: { hasAppHeader = false, ...pageProps } }: AppProps) => {
     const { store, props } = wrapper.useWrappedStore(pageProps);
 
-    return (
+  //  TODO : here detech load
+  const [load, setLoad] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => setLoad(true), 1000)
+  }, [])
+
+  if(!load) return <Splash />
+
+
+  return (
         <Provider store={store}>
             <Layout hasAppHeader={hasAppHeader}>
                 <Component {...props} />
