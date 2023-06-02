e52911c4b239b26368184c6c33509be88fbbc625
JungSWon <sw.onandon@gmail.com>
2023-06-02 18:19:49 +0900

    feat[#30] polish result page

--- client/src/pages/result/index.tsx
+++ client/src/pages/result/index.tsx
@@ -1,28 +1,32 @@
 import { FC, ReactNode } from 'react';
 import { DQButton } from '../../components/reusable/DQButton';
+import styles from '~/pages/result/styles/index.module.scss';
+import Forward from '~/images/caret-forward.svg';
+
+
 
 const Result: FC = () => {
   return <div>
-    <h1>채점 결과를 확인해보세요!</h1>
-    <div>
-      <div>
-        <p>정답</p>
-        <p>3</p>
+    <h1 className={styles.title}>채점 결과를 확인해보세요!</h1>
+    <div className={styles.resultGrid}>
+      <div className={`${styles.block} ${styles.correct}`}>
+        <p className={styles.blockText}>정답</p>
+        <p className={styles.blockNumber}>3</p>
       </div>
-      <div>
-        <p>오답</p>
-        <p>7</p>
+      <div className={`${styles.block} ${styles.wrong}`}>
+        <p className={styles.blockText}>오답</p>
+        <p className={styles.blockNumber}>7</p>
       </div>
     </div>
 
-    <div>
-      <DQButton>홈으로</DQButton>
-      <DQButton>결과 공유하기</DQButton>
-      <DQButton>틀린문제 확인하기</DQButton>
+    <div className={styles.buttonContainer}>
+      <DQButton hasIcon onClick={()=>{}}>홈으로</DQButton>
+      <DQButton hasIcon onClick={()=>{}}>결과 공유하기</DQButton>
+      <DQButton hasIcon onClick={()=>{}}>틀린문제 확인하기</DQButton>
     </div>
-    <div>
+    <div className={styles.inputContainer}>
       <input type="text" placeholder='문제 업데이트 시 알림 받을 이메일을 입력해주세요!' />
-      <p>문제 업데이트 시 알림 받을 이메일을 입력</p>
+      <p>・ 문제 업데이트 시 알림 받을 이메일을 입력</p>
     </div>
   </div>;
 };
