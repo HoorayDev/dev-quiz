8764a9723541c80d3184113bb187c96b98e3c9c2
정수원 <sw.onandon@gmail.com>
2023-06-03 10:39:15 +0900

    Feature/refatcor/#30 (#53)
    * fix[#30] refactor dir
    
    * fix[#30] import

--- client/src/pages/result/index.tsx
+++ client/src/pages/result/index.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode } from 'react';
-import { DQButton } from '../../components/reusable/DQButton';
+import { DQButton } from '~/components/reusable/DQButton';
 
 const Result: FC = () => {
   return <div>
