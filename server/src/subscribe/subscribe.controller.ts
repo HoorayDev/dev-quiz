60e0a2d88e76baea4ac3f08324909576e0a69dd6
2Ruk <94098919+2Ruk@users.noreply.github.com>
2023-06-05 15:41:45 +0900

    docs[#24]: add Create Subscribe docs

--- server/src/subscribe/subscribe.controller.ts
+++ server/src/subscribe/subscribe.controller.ts
@@ -1,29 +1,28 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { SubscribeService } from './subscribe.service';
-import { ApiTags } from '@nestjs/swagger';
-import {
-  CreateSubscribeDto,
-  UpdateSubscribeDto,
-} from '@api/subscribe/dto/subscribe.input.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateResponseDto } from '@app/share-library/dto/response.dto';
 
 @ApiTags('subscribe')
 @Controller('subscribe')
 export class SubscribeController {
   constructor(private readonly subscribeService: SubscribeService) {}
 
-  // TODO: 로그인 체크
-  // TODO: 구독 여부 체크 후 멱등성 유지
+  @ApiOperation({ summary: 'Subscribe Create' })
+  @ApiResponse({
+    status: 201,
+    description: '구독 생성',
+    type: CreateResponseDto,
+    headers: {
+      'Set-Cookie': {
+        description: 'cookie id set',
+      },
+    },
+  })
   @Post()
-  create(@Body() createSubscribeDto: CreateSubscribeDto) {
-    return this.subscribeService.create(createSubscribeDto);
+  create() {
+    // TODO: 로그인 체크
+    // TODO: 구독 여부 체크 후 멱등성 유지
+    return this.subscribeService.create({});
   }
-
 }
