4e0c392eef28796dfcfa4fe653ae7e90d7e83f8f
2Ruk <leerukdev@gmail.com>
2023-06-06 15:21:39 +0900

    feat[#24]: strategy pattern add

--- server/libs/share-library/type/class.interface.ts
+++ server/libs/share-library/type/class.interface.ts
@@ -1,6 +1,6 @@
 export interface RepositoryInterface<T, K, F> {
   findOneWithKey(key: K): T;
-  findOne(filter: F): T;
+  findOneWithFilter(key: K, filter: F): T;
   findAll(filter: F): T[];
   create(createProp: T): T;
   update({ key, prop }: { key: K; prop: T }): T;
