49bcc974a019c3e6fc98616f738e1604e042ce2a
2Ruk <leerukdev@gmail.com>
2023-06-06 13:35:43 +0900

    docs[#24]: quiz-set getOne Api docs add in swagger

--- server/src/quiz-set/quiz-set.controller.ts
+++ server/src/quiz-set/quiz-set.controller.ts
@@ -2,14 +2,17 @@ import { Controller, Get, Param, Query } from '@nestjs/common';
 import { QuizSetService } from './quiz-set.service';
 import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Pagination } from '@app/share-library/dto/request.dto';
-import { ReadAllQuizSetResponseDto } from '@api/quiz-set/type/quiz-set.response.dto';
+import {
+  ReadAllQuizSetResponseDto,
+  ReadOneQuizSetResponseDto,
+} from '@api/quiz-set/type/quiz-set.response.dto';
 @ApiTags('quiz-set')
 @Controller('quiz-set')
 export class QuizSetController {
   constructor(private readonly quizSetService: QuizSetService) {}
 
   // TODO: 기본적인 정보만 뿌려주는 API 화
-  @ApiOperation({ summary: '퀴즈 Set 조회' })
+  @ApiOperation({ summary: '퀴즈 Set 리스트   조회' })
   @ApiQuery({
     name: 'page',
     type: Number,
@@ -33,6 +36,13 @@ export class QuizSetController {
   // TODO: 퀴즈 Set 조회
   // TODO: 닉네임 생성 여부 체크
   // TODO: 확인 이후, 퀴즈 Set 조회 ( Detail, Quiz, Option )
+
+  @ApiOperation({ summary: '퀴즈 Set 단일 조회' })
+  @ApiResponse({
+    status: 200,
+    description: '퀴즈 Set 조회 성공',
+    type: ReadOneQuizSetResponseDto,
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.quizSetService.findOne(+id);
