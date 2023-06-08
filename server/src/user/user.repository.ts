3602f39def96f1c3ea7b20f301ec4d801fe1912c
2Ruk <leerukdev@gmail.com>
2023-06-08 18:58:01 +0900

    refactor[#38]: user repository ( reason: class interface change )

--- server/src/user/user.repository.ts
+++ server/src/user/user.repository.ts
@@ -29,23 +29,32 @@ export class UserRepository
     }
   }
 
-  findAll(filter: UserLogicInterface): UserEntity[] {
-    return [];
+  findAll(filter: UserLogicInterface): Promise<UserEntity[]> {
+    return Promise.resolve([]);
   }
 
-  findOneWithKey(key: string): UserEntity {
-    return undefined;
+  findOneWithFilter(
+    key: string,
+    filter: UserLogicInterface,
+  ): Promise<UserEntity> {
+    return Promise.resolve(undefined);
   }
 
-  remove(key: string): UserEntity {
-    return undefined;
+  findOneWithKey(key: string): Promise<UserEntity> {
+    return Promise.resolve(undefined);
   }
 
-  update({ key, prop }: { key: string; prop: UserEntity }): UserEntity {
-    return undefined;
+  remove(key: string): Promise<string> {
+    return Promise.resolve(undefined);
   }
 
-  findOneWithFilter(key: string, filter: UserLogicInterface): UserEntity {
-    return undefined;
+  update({
+    key,
+    prop,
+  }: {
+    key: string;
+    prop: UserEntity;
+  }): Promise<UserEntity> {
+    return Promise.resolve(undefined);
   }
 }
