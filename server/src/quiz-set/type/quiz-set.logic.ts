987194b95682954318ab74a916f0e9b314161095
2Ruk <leerukdev@gmail.com>
2023-06-09 17:57:06 +0900

    refactor[#24]: quiz-set type refactor

--- server/src/quiz-set/type/quiz-set.logic.ts
+++ server/src/quiz-set/type/quiz-set.logic.ts
@@ -1,4 +1,10 @@
 import { QuizSetEntity } from '@app/share-library/entities/question/quiz-set.entity';
 import { PartialType } from '@nestjs/mapped-types';
+import { Pagination } from '@app/share-library/dto/request.dto';
 
 export class QuizSetLogicInterface extends PartialType(QuizSetEntity) {}
+
+export class QuizSetFilter {
+  pagination: Pagination;
+  filter?: QuizSetLogicInterface;
+}
