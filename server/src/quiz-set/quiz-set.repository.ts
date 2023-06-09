f1b2414c993b0e720db24ab2ff9e4654d5d77702
2Ruk <leerukdev@gmail.com>
2023-06-09 17:59:44 +0900

    feat[#24]: quiz-set read all query init

--- server/src/quiz-set/quiz-set.repository.ts
+++ server/src/quiz-set/quiz-set.repository.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { RepositoryInterface } from '@app/share-library/type/class.interface';
-import { QuizSetLogicInterface } from '@api/quiz-set/type/quiz-set.logic';
+import { QuizSetFilter } from '@api/quiz-set/type/quiz-set.logic';
 import { InjectRepository } from '@nestjs/typeorm';
 import { QuizSetEntity } from '@app/share-library/entities/question/quiz-set.entity';
 import { Repository } from 'typeorm';
+import { plainToClass, plainToInstance } from 'class-transformer';
+import { ReadAllResponseQuizSet } from '@api/quiz-set/type/quiz-set.class.interface';
 
 type QuizSetKey = number;
 
 @Injectable()
 export class QuizSetRepository
-  implements
-    RepositoryInterface<QuizSetEntity, QuizSetKey, QuizSetLogicInterface>
+  implements RepositoryInterface<QuizSetEntity, QuizSetKey, QuizSetFilter>
 {
   constructor(
     @InjectRepository(QuizSetEntity)
@@ -32,13 +33,35 @@ export class QuizSetRepository
     return Promise.resolve(undefined);
   }
 
-  findAll(filter: QuizSetLogicInterface): Promise<QuizSetEntity[]> {
-    return Promise.resolve([]);
+  async findAll({
+    pagination,
+  }: QuizSetFilter): Promise<ReadAllResponseQuizSet> {
+    const { page, limit } = pagination;
+    const offset = (page - 1) * limit;
+
+    try {
+      const [list, count] = await this.quizSetRepository
+        .createQueryBuilder('quizSet')
+        .offset(offset)
+        .limit(limit)
+        .getManyAndCount();
+
+      const totalPage = Math.ceil(count / limit);
+
+      const returnData = {
+        list,
+        count,
+        totalPage,
+        currentPage: page,
+      };
+      return plainToInstance(ReadAllResponseQuizSet, returnData);
+    } catch (e) {}
+    return Promise.resolve(undefined);
   }
 
   findOneWithFilter(
     key: QuizSetKey,
-    filter: QuizSetLogicInterface,
+    filter: QuizSetFilter,
   ): Promise<QuizSetEntity> {
     return Promise.resolve(undefined);
   }
