e444395e20f122e2c3c72b17e97c98f0807c581f
2Ruk <leerukdev@gmail.com>
2023-06-09 17:54:13 +0900

    refactor[#24]: change read all type

--- server/libs/share-library/dto/request.dto.ts
+++ server/libs/share-library/dto/request.dto.ts
@@ -1,15 +1,21 @@
-import { ApiProperty, ApiQuery } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsNumber } from 'class-validator';
 
 export class Pagination {
   @ApiProperty({
     description: '페이지 번호',
     example: 1,
   })
+  @Type(() => Number)
+  @IsNumber()
   page: number;
 
   @ApiProperty({
     description: '페이지당 데이터 수',
     example: 10,
   })
+  @Type(() => Number)
+  @IsNumber()
   limit: number;
 }
--- server/libs/share-library/type/class.interface.ts
+++ server/libs/share-library/type/class.interface.ts
@@ -1,7 +1,14 @@
+export class ReadAllResponse<T> {
+  list: T[];
+  currentPage: number;
+  totalPage: number;
+  count: number;
+}
+
 export interface RepositoryInterface<T, K, F> {
   findOneWithKey(key: K): Promise<T>;
   findOneWithFilter(key: K, filter: F): Promise<T>;
-  findAll(filter: F): Promise<T[]>;
+  findAll(filter: F): Promise<ReadAllResponse<T>>;
   create(createProp: T): Promise<T>;
   update({ key, prop }: { key: K; prop: T }): Promise<T>;
   remove(key: K): Promise<K>;
