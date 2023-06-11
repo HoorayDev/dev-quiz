1fca024ea5c61df1527359fa4f54118408ddb18f
JungSWon <sw.onandon@gmail.com>
2023-06-12 00:06:35 +0900

    feat[#33] add link on appHeader

--- client/src/components/floating/appHeader.tsx
+++ client/src/components/floating/appHeader.tsx
@@ -1,16 +1,19 @@
 import { FC } from "react";
+import { useRouter } from 'next/router';
 import styles from '~/components/floating/appHeader.module.scss';
 import Logo from '~/images/hooray_logo.svg'
+import { HOME, PATCH_NOTE,  } from '~/constants/routing'
 
 interface AppHeaderProps {}
 
 const AppHeader: FC<AppHeaderProps> = () => {
+  const { push } = useRouter();
   return <nav className={`${styles.nav} ${styles.flexRow}`}>
     <Logo className={styles.logo}/>
     <div className={`${styles.flexRow} ${styles.navMenu}`}>
-      <p>홈</p>
+      <p onClick={()=>push(HOME.href)}>홈</p>
       <p>팀소개</p>
-      <p>패치노트</p>
+      <p onClick={()=>push(PATCH_NOTE.href)}>패치노트</p>
     </div>
   </nav>
 }
