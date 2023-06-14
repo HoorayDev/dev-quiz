6b7df1d447365912955ac9ec20d39d4f33ba1d2c
2Ruk <leerukdev@gmail.com>
2023-06-14 14:50:23 +0900

    feat[#24]: import answer module

--- server/src/quiz-response/quiz-response.module.ts
+++ server/src/quiz-response/quiz-response.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { QuizResponseService } from './quiz-response.service';
 import { QuizResponseController } from './quiz-response.controller';
+import { AnswerModule } from '@api/answer/answer.module';
 
 @Module({
+  imports: [AnswerModule],
   controllers: [QuizResponseController],
-  providers: [QuizResponseService]
+  providers: [QuizResponseService],
 })
 export class QuizResponseModule {}
