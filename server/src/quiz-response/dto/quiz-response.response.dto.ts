0710e4b144addf00105dc8b970cb80970aff1529
2Ruk <leerukdev@gmail.com>
2023-06-14 17:44:12 +0900

    refactor[#24]: response input, response dto

--- server/src/quiz-response/dto/quiz-response.response.dto.ts
+++ server/src/quiz-response/dto/quiz-response.response.dto.ts
@@ -12,4 +12,30 @@ export class QuizVoteResponseDto {
     example: 5,
   })
   inCorrectCount: number;
+
+  @ApiProperty({
+    description: '퀴즈 총 개수',
+    example: 10,
+  })
+  totalCount: number;
+
+  @ApiProperty({
+    description: '퀴즈별 정답 여부',
+    example: {
+      1: true,
+      2: false,
+      3: true,
+    },
+  })
+  correctMap: { [key: string]: boolean };
+
+  @ApiProperty({
+    description: '정답률',
+  })
+  correctPercentage: number;
+
+  @ApiProperty({
+    description: '오답률',
+  })
+  inCorrectPercentage: number;
 }
