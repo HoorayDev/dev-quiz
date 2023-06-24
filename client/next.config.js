a26c661dc25fbe544dcc03cfa8db38945fd18a44
정수원 <sw.onandon@gmail.com>
2023-06-24 15:30:56 +0900

    feat [#33] cloudfront image (#76)

--- client/next.config.js
+++ client/next.config.js
@@ -2,13 +2,13 @@
 const path = require('path')
 
 const nextConfig = {
-    image:{
+    images:{
         // limit of 25 deviceSizes values
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         // limit of 25 imageSizes values
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
         // limit of 50 domains values
-        domains: ['http://localhost:3000'],
+        domains: ['http://localhost:3000','d32kg0ommnjq4u.cloudfront.net'],
         // path prefix for Image Optimization API, useful with `loader`
         path: '/_next/image',
         // loader can be 'default', 'imgix', 'cloudinary', 'akamai', or 'custom'
