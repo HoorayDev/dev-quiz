1810aa83463ec04dc8ea1e1b4143a5e79e4a4290
JungSWon <sw.onandon@gmail.com>
2023-07-06 17:32:58 +0900

    feat[#33]: create & apply footer

--- client/src/components/HOC/Layout.tsx
+++ client/src/components/HOC/Layout.tsx
@@ -1,20 +1,23 @@
 import { ReactNode } from 'react';
 import styles from '~/components/HOC/layout.module.scss';
 import { AppHeader } from '~/components/floating/appHeader';
+import { DQFooter } from '~/components/reusable/DQFooter'
 import cn from "clsx";
 
 interface LayoutProps {
     children : ReactNode;
     hasAppHeader : boolean;
+    hasAppFooter : boolean;
 }
 
-const Layout = ({children, hasAppHeader} : LayoutProps) => {
+const Layout = ({children, hasAppHeader, hasAppFooter} : LayoutProps) => {
     return (
         <>
         {hasAppHeader && <AppHeader/>}
         <div className={styles.wrapper}>
             <div className={cn(styles.layout, hasAppHeader ? styles.hasAppHeader : styles.withoutAppHeader )}>{children}</div>
         </div>
+        {hasAppFooter && <DQFooter/>}
         </>
     )
 }
