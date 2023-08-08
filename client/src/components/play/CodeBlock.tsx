3d3669dfbeb122cb028ff0357ec5e3bbde1a7644
Jay <85284246+Yu-jae-min@users.noreply.github.com>
2023-08-08 23:38:35 +0900

    feat[#110]: fix responsive break point

--- client/src/components/play/CodeBlock.tsx
+++ client/src/components/play/CodeBlock.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState, useEffect, useMemo } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { prism, oneLight } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import { merge } from 'lodash';
@@ -8,12 +8,44 @@ interface CodeBlockProps {
   code: string;
 }
 
-const customStyle = {
+const defaultStyle = {
   width: '100%',
   marginTop: '60px',
 }
 
+const responsiveStyle = {
+  mobile: {
+    ...defaultStyle,
+    fontSize: '14px',
+  }
+}
+
 const CodeBlock: FC<CodeBlockProps> = ({ language = 'javascript', code }) => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(function registerMediaQuery(){
+    const breakPoint = '(max-width: 768px)'
+    const mediaQuery = window.matchMedia(breakPoint);
+
+    const handleResize = () => {
+      setIsSmallScreen(mediaQuery.matches);
+    };
+
+    mediaQuery.addListener(handleResize);
+
+    return () => {
+      mediaQuery.removeListener(handleResize);
+    };
+  }, []);
+
+  const customStyle = useMemo(() => {
+    if(isSmallScreen){
+      return defaultStyle
+    }
+
+    return responsiveStyle.mobile
+  }, [isSmallScreen])
+
   return (
     <SyntaxHighlighter language={language} style={prism} customStyle={customStyle}>
       {code}
